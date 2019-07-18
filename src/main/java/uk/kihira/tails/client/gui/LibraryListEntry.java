dfb38f9103a137b60a05608cd21517bd8a5a15ee
DirkPC <d.kok.2000@gmail.com>
2019-07-18 21:48:31 +0200

    fixed errors in client.gui.LibraryListEntry

--- src/main/java/uk/kihira/tails/client/gui/LibraryListEntry.java
+++ src/main/java/uk/kihira/tails/client/gui/LibraryListEntry.java
@@ -4,6 +4,7 @@ import com.mojang.authlib.GameProfile;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.FontRenderer;
 import net.minecraft.client.gui.GuiListExtended;
+import net.minecraft.client.gui.IGuiEventListener;
 import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.client.resources.I18n;
 import net.minecraft.util.text.TextFormatting;
@@ -11,12 +12,14 @@ import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.fml.client.config.GuiUtils;
 import uk.kihira.tails.client.OutfitPart;
+import uk.kihira.tails.client.Part;
 import uk.kihira.tails.client.PartRegistry;
 import uk.kihira.tails.common.LibraryEntryData;
+import uk.kihira.tails.common.OutfitManager;
 import uk.kihira.tails.common.Tails;
 
 @OnlyIn(Dist.CLIENT)
-public class LibraryListEntry implements GuiListExtended.IGuiListEntry {
+public class LibraryListEntry extends GuiListExtended.IGuiListEntry implements IGuiEventListener {
 
     public final LibraryEntryData data;
 
@@ -25,52 +28,42 @@ public class LibraryListEntry implements GuiListExtended.IGuiListEntry {
     }
 
     @Override
-    public void drawEntry(int slotIndex, int x, int y, int listWidth, int slotHeight, int mouseX, int mouseY, boolean isSelected, float partialTicks) {
+    public void drawEntry(int listWidth, int slotHeight, int x, int y, boolean isSelected, float partialTicks) {
         if (data.remoteEntry) {
-            Minecraft.getMinecraft().renderEngine.bindTexture(GuiIconButton.ICONS_TEXTURES);
+            Minecraft.getInstance().textureManager.bindTexture(GuiIconButton.ICONS_TEXTURES);
             GuiIconButton.Icons icon = GuiIconButton.Icons.SERVER;
             GlStateManager.pushMatrix();
-            GlStateManager.translate(x + listWidth - 16, y + slotHeight - 12, 0F);
-            GlStateManager.scale(0.8F, 0.8F, 1F);
+            GlStateManager.translatef(x + listWidth - 16, y + slotHeight - 12, 0F);
+            GlStateManager.scalef(0.8F, 0.8F, 1F);
             GuiUtils.drawTexturedModalRect(0, 0, icon.u, icon.v, 16, 16, 10);
             GlStateManager.popMatrix();
         }
 
-        FontRenderer fontRenderer = Minecraft.getMinecraft().fontRenderer;
+        FontRenderer fontRenderer = Minecraft.getInstance().fontRenderer;
         fontRenderer.drawString((data.outfit.equals(Tails.localOutfit) ? TextFormatting.GREEN + "" + TextFormatting.ITALIC : "") + data.entryName, 5, y + 3, 0xFFFFFF);
 
-        fontRenderer.setUnicodeFlag(true);
         int offset = 0;
-        for (OutfitPart part : data.outfit.parts) {
-            fontRenderer.drawString(I18n.format(PartRegistry.getPart(part.basePart).name), x + 5, y + 12 + (8 * offset), 0xFFFFFF);
-            for (int i = 1; i < 4; i++) {
-                //todo Gui.drawRect(listWidth - (8 * i), y + 13 + (offset * 8), listWidth + 7 - (8 * i), y + 20 + (offset * 8), part.tint[i - 1]);
-            }
-            offset++;
+        for (OutfitPart outfitPart : data.outfit.parts) {
+            Part part = PartRegistry.getPart(outfitPart.basePart);
+            if (part != null) {
+                fontRenderer.drawString(I18n.format(part.name), x + 5, y + 12 + (8 * offset), 0xFFFFFF);
+                //for (int i = 1; i < 4; i++) {
+                //    todo Gui.drawRect(listWidth - (8 * i), y + 13 + (offset * 8), listWidth + 7 - (8 * i), y + 20 + (offset * 8), part.tint[i - 1]);
+                //}
+                offset++;
+            } // else continue;
         }
-        fontRenderer.setUnicodeFlag(false);
 
         if (data.favourite) {
-            Minecraft.getMinecraft().renderEngine.bindTexture(GuiIconButton.ICONS_TEXTURES);
+            Minecraft.getInstance().textureManager.bindTexture(GuiIconButton.ICONS_TEXTURES);
             GuiIconButton.Icons icon = GuiIconButton.Icons.STAR;
             GlStateManager.pushMatrix();
-            GlStateManager.translate(x + listWidth - 16, y, 0F);
-            GlStateManager.scale(0.8F, 0.8F, 1F);
+            GlStateManager.translatef(x + listWidth - 16, y, 0F);
+            GlStateManager.scalef(0.8F, 0.8F, 1F);
             GuiUtils.drawTexturedModalRect(0, 0, icon.u, icon.v + 32, 16, 16, 10);
             GlStateManager.popMatrix();
         }
     }
-
-    @Override
-    public void updatePosition(int p_192633_1_, int p_192633_2_, int p_192633_3_, float p_192633_4_) {}
-
-    @Override
-    public boolean mousePressed(int index, int mouseX, int mouseY, int mouseEvent, int mouseSlotX, int mouseSlotY) {
-        return false;
-    }
-
-    @Override
-    public void mouseReleased(int index, int mouseX, int mouseY, int mouseEvent, int mouseSlotX, int mouseSlotY) {}
 
     public static class NewLibraryListEntry extends LibraryListEntry {
 
@@ -82,16 +75,17 @@ public class LibraryListEntry implements GuiListExtended.IGuiListEntry {
         }
 
         @Override
-        public void drawEntry(int slotIndex, int x, int y, int listWidth, int slotHeight, int mouseX, int mouseY, boolean isSelected, float partialTicks) {
-            Minecraft.getMinecraft().fontRenderer.drawString(I18n.format("gui.library.create"), x + 3, y + (slotHeight / 2) - 4, 0xFFFFFF);
+        public void drawEntry(int listWidth, int slotHeight, int x, int y, boolean isSelected, float partialTicks) {
+            Minecraft.getInstance().fontRenderer.drawString(I18n.format("gui.library.create"), x + 3, y + (slotHeight / 2) - 4, 0xFFFFFF);
         }
 
         @Override
-        public boolean mousePressed(int index, int mouseX, int mouseY, int mouseEvent, int mouseSlotX, int mouseSlotY) {
+        public boolean mouseClicked(double x, double y, int modifiers) {
+        //public boolean mousePressed(int index, int mouseX, int mouseY, int mouseEvent, int mouseSlotX, int mouseSlotY) {
             //Create entry and add to library
-            GameProfile profile = Minecraft.getMinecraft().player.getGameProfile();
+            GameProfile profile = Minecraft.getInstance().player.getGameProfile();
             LibraryEntryData data = new LibraryEntryData(profile.getId(), profile.getName(), I18n.format("gui.library.entry.default"), Tails.localOutfit);
-            Tails.proxy.getLibraryManager().addEntry(data);
+            OutfitManager.INSTANCE.getLibraryManager().addEntry(data);
             panel.addSelectedEntry(new LibraryListEntry(data));
             return false;
         }
